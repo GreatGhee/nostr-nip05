394fc0526e8d8139684e2612486126908b3e914c
GreatGhee <patrickomalley3@gmail.com>
2025-09-20 15:22:25 -0400

    Update products.js

--- js/products.js
+++ js/products.js
@@ -17,7 +17,7 @@ const products = [
         buttonId: 'buyButtonLiver',
         imageSliderId: 'sliderLiver',
         options: [
-            { size: '1-pack', price: 26, front: 'images/4ozCrispsFront.jpg', back: 'images/4ozCrispsBack.jpg' }
+            { size: '1-pack (3 oz)', price: 26, front: 'images/4ozCrispsFront.jpg', back: 'images/4ozCrispsBack.jpg' }
             { size: '3-pack', price: 70, front: 'images/4ozCrispsFront.jpg', back: 'images/4ozCrispsBack.jpg' }
             { size: '5-pack', price: 110, front: 'images/4ozCrispsFront.jpg', back: 'images/4ozCrispsBack.jpg' }
         ]
